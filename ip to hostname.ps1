6b24a835e8c5c675c02869548c3ea7fd5068544b
danvarela <dvarela@novacoast.com>
2022-07-25 08:16:11 -0500

    Add files via upload

---
+++ ip to hostname.ps1
@@ -0,0 +1,9 @@
+﻿Import-Csv C:ip-Addresses.csv | ForEach-Object{
+$hostname = ([System.Net.Dns]::GetHostByAddress($_.IPAddress)).Hostname
+if($? -eq $False){
+$hostname="Cannot resolve hostname"
+}
+New-Object -TypeName PSObject -Property @{
+      IPAddress = $_.IPAddress
+      HostName = $hostname
+}} | Export-Csv C:machinenames.csv -NoTypeInformation -Encoding UTF8
---
+++ iptest.ps1
@@ -0,0 +1,34 @@
+﻿ Get-Content ".\ip-Addresses.csv" |
+     ForEach-Object{
+         $result = [ordered]@{
+                     DNSName     = $_.Trim()
+                     HostName    = ""
+                     DNSIPv4     = ""
+                     Up          = ""
+                     TestedIPv4  = ""
+                     }
+         Try{
+             $entry = [System.Net.Dns]::GetHostEntry($_.Trim())
+             $result.HostName = $entry.HostName
+             $entry.AddressList |
+                 Where-Object {$_.AddressFamily -eq 'InterNetwork'} |
+                     ForEach-Object{
+                         $result.DNSIPv4 = $_.IPAddressToString
+                         [array]$x = Test-Connection -Delay 15 -ComputerName $result.DNSName -Count 1 -ErrorAction SilentlyContinue
+                         if ($x){
+                             $result.Up = "Yes"
+                             $result.TestedIPv4 = $x[0].IPV4Address
+                         }
+                         else{
+                             $result.Up = "No"
+                             $result.TestedIPv4 = "N/A"
+                         }
+                     }
+         }
+         Catch{
+             $result.HostName = "Host Unknown"
+             $result.Up = "Unknown"
+             $result.TestedIPv4 = "N/A"
+         }
+         [PSCustomObject]$result
+     } | Export-Csv C:output.csv -NoTypeInformation -Encoding UTF8
